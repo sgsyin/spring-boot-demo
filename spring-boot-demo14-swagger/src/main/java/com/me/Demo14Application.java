0727dd4024a851e9f11ce0671bd1c46d28a4a290
sgsyin <3329477983@qq.com>
2023-02-12 14:02:22 +0800

    新增文档（swagger）

---
+++ spring-boot-demo14-swagger/src/main/java/com/me/Demo14Application.java
@@ -0,0 +1,13 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo14Application.class, args);
+    }
+
+}
